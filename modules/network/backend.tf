83bb8d5866d2a6fe8d613487e843068e345919f6
Jake Furlong <jake.w.furlong@gmail.com>
2022-05-17 15:58:00 -0600

    cleanup and adding gitignore file

--- modules/network/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "kubernimbus-state"
-    key            = "global/s3/network/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "kubernimbus-network-locks"
-    encrypt        = true
-  }
-}
